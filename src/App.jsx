f7f823665293fb62ff6f7fd7658bacad0208dee4
Md. Pieash Ahmed <pieash9@gmail.com>
2025-05-21 07:16:45 +0600

    refactor contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import MovieList from "./cine/MovieList";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
 const App = () => {
-  return <div>App test</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          {/* Sidebar */}
+          <Sidebar />
+          {/* Content */}
+          <MovieList />
+        </div>
+      </main>
+
+      <Footer />
+    </>
+  );
 };
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: "/movie-rental-portal",
   plugins: [react(), tailwindcss()],
-  base: "/movie-rental-portal/",
 });
